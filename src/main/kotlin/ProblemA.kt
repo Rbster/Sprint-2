9d5774fb2ba51b387613208bcea859d331f05e95
Rbster <Rbster@yandex.ru>
2021-08-24 01:12:43 +0300

    problemA not tested

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,6 +1,26 @@
 class ProblemA {
 
     fun main() {
+        var t: Int
+        var a: Long
+        var b: Long
+        var k: Long
+        var position:Long
+        var listLong: List<Long>
+
+        t = readInt()
+
+        for (i in 1..t) {
+            listLong = readListLong()
+            a = listLong[0]
+            b = listLong[1]
+            k = listLong[2]
+            position = 0L
+            for (j in 1..k) {
+                position = if ((j - 1L) % 2L == 0L) position + a else position - b
+            }
+            println(position)
+        }
 
         //ваша реализация в методе main
 
