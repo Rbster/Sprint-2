f1d9169eda9858eddccc51dcca4b0b522d5a3222
Rbster <Rbster@yandex.ru>
2021-08-24 09:18:23 +0300

    problemB not tested

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -1,7 +1,23 @@
 class ProblemB {
-
     fun main() {
+        var t: Int
+        var n: Int
+        var k: Int
+        var char = 'a'
+        var resultBuilder: StringBuilder
+        fun min(a: Int, b: Int) = if (a > b) b else a
 
+        t = readInt()
+
+        for (i in 1..t) {
+            resultBuilder = StringBuilder()
+            n = readInt()
+            k = readInt()
+            for (j in 0 until min(n, k)) {
+                resultBuilder.append((char.toInt() + j % 26).toChar())
+            }
+            println(resultBuilder.toString())
+        }
         //ваша реализация в методе main
 
     }
