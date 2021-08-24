02b7260c6416621fd028a86c01f898294df0d898
Rbster <Rbster@yandex.ru>
2021-08-24 11:11:11 +0300

    A, C tested. B wrong tests

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -3,17 +3,19 @@ class ProblemB {
         var n: Int
         var k: Int
         var resultBuilder: StringBuilder
+        var inputArray: IntArray
 
         val char = 'a'
         val t = readInt()
         fun min(a: Int, b: Int) = if (a > b) b else a
 
         for (i in 1..t) {
-            resultBuilder = StringBuilder()
-            n = readInt()
-            k = readInt()
-            for (j in 0 until min(n, k)) {
-                resultBuilder.append((char.code + j % 26).toChar())
+            resultBuilder = StringBuilder("")
+            inputArray = readIntArray(2)
+            n = inputArray[0]
+            k = inputArray[1]
+            for (j in 0 until n) {
+                resultBuilder.append((char.code + j % k).toChar())
             }
             println(resultBuilder.toString())
         }
--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -1,22 +1,16 @@
 class ProblemC {
 
     fun main() {
-        var n: Int
-        var studentArray: IntArray
-        var result: Int
+        val n = readInt()
+        val studentArray = readIntArray(n)
+        var result = 0
 
-        val t = readInt()
-
-        for (i in 1..t) {
-            n = readInt()
-            studentArray = readIntArray(n)
-            studentArray.sort()
-            result = 0
-            for (j in 1 until n step 2) {
-                result += studentArray[j] - studentArray[j - 1]
-            }
-            println(result)
+        studentArray.sort()
+        for (j in 1 until n step 2) {
+            result += studentArray[j] - studentArray[j - 1]
         }
+        println(result)
+
 
         //ваша реализация в методе main
 
